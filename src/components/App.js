ca08241c2feefc67e49a1073da6429e47a7e66d3
Bart <korysiu@gmail.com>
2019-11-13 22:59:44 +0100

    Added forecast weather

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,29 @@ import Result from './Result';
 import './App.css';
 const APIKey = 'b7129e529853a3632061695c4b98dde7';
 
+//--Funkcja wyszukująca w tablicy prognozę na godz 12 kolejnego dnia--//  
+const getForecastDay = (list) => {
+  let dayName, hour;
+  const index = list.findIndex((day) => {
+    dayName = new Date(day.dt_txt);
+    hour = dayName.getHours();
+    day = dayName.getDay();
+    return (hour === 12 & day !== 1);
+  });
+  return index;
+}
+//--Funkcja wyszukująca w tablicy prognozę na godz 3 kolejnego dnia--//
+const getForecastNight = (list) => {
+  let dayName, hour;
+  const index = list.findIndex((day) => {
+    dayName = new Date(day.dt_txt);
+    hour = dayName.getHours();
+    day = dayName.getDay();
+    return (hour === 3 & day !== 1);
+  });
+  return index;
+}
+
 class App extends Component {
   state = {
     value: '',
@@ -18,6 +41,18 @@ class App extends Component {
     humidity: '',
     description:'',
     img: '',
+    dayNameOne:'',
+    forecastFirstDay: '',
+    forecastFirstNight: '',
+    dayNameTwo: '',
+    forecastSecondDay: '',
+    forecastSecondNight: '',
+    dayNameThree:'',
+    forecastThirdDay: '',
+    forecastThirdNight: '',
+    dayNameFour:'',
+    forecastFourthDay: '',
+    forecastFourthNight: '',
     err: false,
   }
 
@@ -32,14 +67,18 @@ class App extends Component {
   handleCitySubmit = (e) => {
     e.preventDefault()
 
-//--Adres API--//
-    const API =`http://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&APPID=${APIKey}&units=metric`;
+//--Adres API aktulnej pogody--//
+    //const APIa =`http://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&APPID=${APIKey}&units=metric`;
 
+ //--Adres API przewidywanej pogody--//
+    const APIf =`http://api.openweathermap.org/data/2.5/forecast?q=${this.state.value}&APPID=${APIKey}&units=metric`;
+
+    
 //--Wysyłanie żądania pod wskazany adres API(async)--//
 //--fetch tworzy promise--//
 //--promise ok-> .then; promise not ok-> .catch--//
 //-- if promise is ok-> response
-    fetch(API)
+    fetch(APIf)
     .then(response => {
       if (response.ok) {//--sprawdza czy response jest ok--//
         return response//--jeśli true(200), przekazuje dalej response--//
@@ -47,20 +86,35 @@ class App extends Component {
       throw Error("Valid data")//--jeśli false(404,500), zwraca error--//
     })
     .then(response => response.json())//--funkcja wyodrębnia plik json--//
-    //.then(data => console.log(data))
     .then(data => {//--obiekt--//
+      console.log(data)
       const time = new Date().toLocaleString();//--Aktualna data i godzina--//
+      const dayList = data.list;//--Obsługa forecastWeather--//
+      const dayIndex = getForecastDay(dayList);//-- jw --//
+      const nightIndex = getForecastNight(dayList);//-- jw --//
       this.setState(state => ({
         err: false,
         date: time,
-        img: data.weather[0].icon,
-        description: data.weather[0].description,
-        sunrise: data.sys.sunrise,
-        sunset: data.sys.sunset,
-        temp: data.main.temp,
-        humidity: data.main.humidity,
-        pressure: data.main.pressure,
-        wind: data.wind.speed,
+        img: data.list[0].weather[0].icon,
+        description: data.list[0].weather[0].description,
+        sunrise: data.city.sunrise,
+        sunset: data.city.sunset,
+        temp: data.list[0].main.temp,
+        humidity: data.list[0].main.humidity,
+        pressure: data.list[0].main.pressure,
+        wind: data.list[0].wind.speed,
+        dayNameOne: data.list[dayIndex].dt_txt,
+        forecastFirstDay: data.list[dayIndex].main.temp,
+        forecastFirstNight: data.list[nightIndex].main.temp,
+        dayNameTwo: data.list[dayIndex+8].dt_txt,
+        forecastSecondDay: data.list[dayIndex+8].main.temp,
+        forecastSecondNight: data.list[nightIndex+8].main.temp,
+        dayNameThree: data.list[dayIndex+16].dt_txt,
+        forecastThirdDay: data.list[dayIndex+16].main.temp,
+        forecastThirdNight: data.list[nightIndex+16].main.temp,
+        dayNameFour: data.list[dayIndex+24].dt_txt,
+        forecastFourthDay: data.list[dayIndex+24].main.temp,
+        forecastFourthNight: data.list[nightIndex+24].main.temp,
         city: state.value,
       }))
     })
@@ -82,5 +136,4 @@ class App extends Component {
     );
   }
 }
-  
 export default App;
--- src/components/Result.js
+++ src/components/Result.js
@@ -2,49 +2,101 @@ import React from 'react';
 
 const Result = props => {
 
-    const {city, sunrise, sunset, temp, humidity, pressure, wind, description, img, err} = props.weather;
-
+    const {
+        city, 
+        sunrise, 
+        sunset, 
+        temp, 
+        humidity, 
+        pressure, 
+        wind, 
+        description, 
+        img, 
+        dayNameOne, 
+        forecastFirstDay,
+        forecastFirstNight,
+        dayNameTwo,
+        forecastSecondDay,
+        forecastSecondNight,
+        dayNameThree,
+        forecastThirdDay,
+        forecastThirdNight,
+        dayNameFour,
+        forecastFourthDay,
+        forecastFourthNight,
+        err} = props.weather;
 //--Startowy stan aplikacji--//
     let content = null;
-
+//--Dane potrzebne do prawidłowego wyświetlania prognozy--//
+    var myDateOne = new Date(dayNameOne);
+    var myDateTwo = new Date(dayNameTwo);
+    var myDateThree = new Date(dayNameThree);
+    var myDateFour = new Date(dayNameFour);
+    let thatDayOne = myDateOne.getDay();
+    let thatDayTwo = myDateTwo.getDay();
+    let thatDayThree = myDateThree.getDay();
+    let thatDayFour = myDateFour.getDay();
+    var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+//--Zaokrąglenia--//
     var number = temp;
     var roundedNumber = Math.round(number, 0);
 
     var windy = wind;
     var roundedWind = Math.round(windy, 0);
-
-
 //--Jeżeli nie ma błędu i jest podane city to...--//
     if (!err && city) {
         const sunriseTime = new Date(sunrise * 1000).toLocaleTimeString()
         const sunsetTime = new Date(sunset * 1000).toLocaleTimeString()
-        
+//--Adres do grafik--//
         let image = 'http://openweathermap.org/img/wn/'+ img +'@2x.png';
 
         content = (
             <>
-                <section className="city-section col">
-                    <p className="h1 text-capitalize"><dt>{city}</dt></p>
-                    <div className="row">
-                        <p className="temperature col text-center">{roundedNumber} &#176;C</p>
-                        <p className="image col text-center"><img src={image} alt={description}></img></p>
+                <section className="row">
+                    <div className="city-section col">
+                        <p className="h1 text-capitalize"><dt>{city}</dt></p>
+                        <div className="row">
+                            <p className="temperature col text-center">{roundedNumber} &#176;C</p>
+                            <p className="image col text-center"><img src={image} alt={description}></img></p>
+                        </div>
+                    </div>
+                    <div className="details col-md">
+                        <h5>Wind: {roundedWind} m/s</h5>
+                        <h5>Humidity: {humidity} %</h5>
+                        <h5>Pressure: {pressure} hPa</h5>
+                        <h5>Sunrise: {sunriseTime}</h5>
+                        <h5>Sunset: {sunsetTime}</h5>
                     </div>
                 </section>
-                <section className="details col-md">
-                    <h5>Wind: {roundedWind} m/s</h5>
-                    <h5>Humidity: {humidity} %</h5>
-                    <h5>Pressure: {pressure} hPa</h5>
-                    <h5>Sunrise: {sunriseTime}</h5>
-                    <h5>Sunset: {sunsetTime}</h5>
+                <section className="forecast-weather">
+                    <div className="one">
+                        <p>{days[thatDayOne]}</p>
+                        <p>{Math.round(forecastFirstDay, 0)} &#176;C</p>
+                        <p>{Math.round(forecastFirstNight, 0)} &#176;C</p>
+                    </div>
+                    <div className="two">
+                        <p>{days[thatDayTwo]}</p>
+                        <p>{Math.round(forecastSecondDay, 0)} &#176;C</p>
+                        <p>{Math.round(forecastSecondNight, 0)} &#176;C</p>
+                    </div>
+                    <div className="three">
+                        <p>{days[thatDayThree]}</p>
+                        <p>{Math.round(forecastThirdDay, 0)} &#176;C</p>
+                        <p>{Math.round(forecastThirdNight, 0)} &#176;C</p>
+                    </div>
+                    <div className="four">
+                        <p>{days[thatDayFour]}</p>
+                        <p>{Math.round(forecastFourthDay, 0)} &#176;C</p>
+                        <p>{Math.round(forecastFourthNight, 0)} &#176;C</p>
+                    </div>
                 </section>
             </>
         )
     }
-
 //--Jeżeli err jest f":"<-(lub)t...--//
 //--"city" pobiera z .catch--//
     return (
-        <div className="result container text-center row"> 
+        <div className="result container text-center"> 
             {err ? `Not found ${city}` : content} 
         </div>
     );
